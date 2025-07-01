1fdeff656440182308d1e74ea134f0db6684c6ca
Mestniy183 <ken-zero@ya.ru>
2025-07-01 13:31:07 +0300

    js

--- js/loadServer.js
+++ js/loadServer.js
@@ -68,12 +68,12 @@ export async function loadServer() {
             questionsList.append(li)
         })
 
-        document.querySelectorAll('.delete-btn').forEach(btn => {
-            btn.addEventListener('click', async () => {
-                if(confirm('Вы уверены, что хотите удалить эту услугу?')){
-                    await deleteService(btn.dataset.id);
-                }
-            })
+        document.querySelectorAll('.services__list .delete-btn').forEach(btn => {
+            btn.addEventListener('click', () => deleteItem(btn.dataset.id, 'services', 'услугу') )
+        })
+
+        document.querySelectorAll('.question__list .delete-btn').forEach(btn => {
+            btn.addEventListener('click', () => deleteItem(btn.dataset.id, 'questions', 'вопрос') )
         })
     } catch (error) {
         console.error('Ошибка загрузки услуг:', error);
@@ -82,24 +82,29 @@ export async function loadServer() {
     }
 }
 
-async function deleteService(serviceId){
+async function deleteItem(itemId, tableName, itemName){
     try {
+
+        if(!confirm(`Вы уверены, что хотите удалить этот ${itemName}?`)){
+            return;
+        }
+
         const user = await getUser()
 
         const{error} = await supabaseDB
-        .from('services')
+        .from(tableName)
         .delete()
-        .eq('id', serviceId)
+        .eq('id', itemId)
         .eq('user_id', user.id);
 
         if(error) throw error;
 
         //Обновляем список после удаления
         await loadServer();
-        alert('Услуга успешно удалена');
+        alert(`${itemName.charAt(0).toUpperCase() + itemName.slice(1)} Успешно удалён`);
 
     } catch (error) {
         console.error('Ошибка удаления:', error);
-        alert('Не удалось удалить услугу');
+        alert(`Не удалось удалить услугу ${itemName}`);
     }
 }
