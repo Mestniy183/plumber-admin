1b0deff5796c7ff88cd6a04c7e48e65af270a79e
mestniy183 <133644102+Mestniy183@users.noreply.github.com>
2025-07-16 11:11:07 +0000

    delete images

--- src/js/loadServer/deleteItem.js
+++ src/js/loadServer/deleteItem.js
@@ -1,30 +1,77 @@
-import { supabaseDB } from "../api.js";
+import { DeleteObjectCommand } from "@aws-sdk/client-s3";
+import { client, supabaseDB } from "../api.js";
 import { getUser } from "../checkAuth.js";
 import { loadServer } from "./loadServer.js";
 
-export async function deleteItem(itemId, tableName, itemName){
-    try {
-
-        if(!confirm(`Вы уверены, что хотите удалить этот ${itemName}?`)){
-            return;
-        }
-
-        const user = await getUser()
-
-        const{error} = await supabaseDB
-        .from(tableName)
-        .delete()
-        .eq('id', itemId)
-        .eq('user_id', user.id);
-
-        if(error) throw error;
-
-        //Обновляем список после удаления
-        await loadServer();
-        alert(`${itemName.charAt(0).toUpperCase() + itemName.slice(1)} Успешно удалён`);
-
-    } catch (error) {
-        console.error('Ошибка удаления:', error);
-        alert(`Не удалось удалить услугу ${itemName}`);
+export async function deleteItem(itemId, tableName, itemName) {
+  try {
+    if (!confirm(`Вы уверены, что хотите удалить этот ${itemName}?`)) {
+      return;
     }
+
+    const user = await getUser();
+    const { data: item, error: fetchError } = await supabaseDB
+      .from(tableName)
+      .select("*")
+      .eq("id", itemId)
+      .eq("user_id", user.id)
+      .single();
+
+    if (fetchError) throw fetchError;
+
+    if (item.image) {
+      const key = extractKeyFromUrl(item.image, "comment");
+
+      const deleteParams = {
+        Bucket: "comment",
+        Key: key,
+      };
+      try {
+        const deleteCommand = new DeleteObjectCommand(deleteParams);
+        await client.send(deleteCommand);
+      } catch (error) {
+        console.error("Ошибка удаления картинки");
+      }
+    }
+
+    if (item.imageBefore && item.imageAfter) {
+      const imagesExample = [item.imageBefore, item.imageAfter];
+
+      for (const image of imagesExample) {
+        const key = extractKeyFromUrl(image, "example");
+
+        const deleteParams = {
+          Bucket: "example",
+          Key: key,
+        };
+        try {
+          const deleteCommand = new DeleteObjectCommand(deleteParams);
+          await client.send(deleteCommand);
+        } catch (error) {
+          console.error("Ошибка удаления картинки");
+        }
+      }
+    }
+
+    const { error } = await supabaseDB
+      .from(tableName)
+      .delete()
+      .eq("id", itemId)
+      .eq("user_id", user.id);
+
+    if (error) throw error;
+
+    //Обновляем список после удаления
+    await loadServer();
+    alert(
+      `${itemName.charAt(0).toUpperCase() + itemName.slice(1)} Успешно удалён`
+    );
+  } catch (error) {
+    console.error("Ошибка удаления:", error);
+    alert(`Не удалось удалить услугу ${itemName}`);
+  }
+}
+function extractKeyFromUrl(url, bucket) {
+  const parts = url.split(`/object/public/${bucket}/`);
+  return parts[1];
 }
